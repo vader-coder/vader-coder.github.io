8b5b23b56459630ab93e3c785e5ae9237b20297c
vader-coder <57960365+vader-coder@users.noreply.github.com>
2020-04-09 18:39:03 -0400

    Update check-password-2.js

--- check-password-2.js
+++ check-password-2.js
@@ -1,14 +1,11 @@
-function CheckPassword(inputtxt) 
-{ 
-var passw = "TrussLab";///^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-if(inputtxt.value.match(passw)) 
-{ 
-alert('Correct, try another...')
-return true;
-}
-else
-{ 
-alert('Wrong...!')
-return false;
-}
+function CheckPassword(inputtxt) { 
+  var passw = "TrussLab";///^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+  if(inputtxt.value.match(passw)) { 
+    alert('Correct, try another...')
+    return true;
+  }
+  else { 
+    alert('Wrong...!')
+    return false;
+  }
 }
