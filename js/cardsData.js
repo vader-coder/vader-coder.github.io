68e725e0e75b868159e4c3c5b4dcb1d27e86c6e5
Patrick Wheeler <pwheele1@swarthmore.edu>
2021-11-07 20:44:15 -0500

    s

---
+++ js/cardsData.js
@@ -0,0 +1,107 @@
+let cardsData = [
+  {
+  id: 0,
+  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  user: {
+    name: 'Okinami',
+    handle: 'twitterid',
+    title: 'BodeRoot'
+  },
+  content: 'Website uses HTML, CSS, Client-Side JavaScript to make Bode Plots.',
+  links: [
+    {
+      link: 'https://lpsa.swarthmore.edu/Bode/bodeDraw.html',
+      text: 'Production Site'//,
+      //indent: '<br/>'
+    },
+    {
+      link: 'https://vader-coder.github.io/BodeRoot/',
+      text: 'Development Site'//,
+      //indent: '<br/>'
+    },
+    {
+      link: 'https://github.com/vader-coder/BodeRoot',
+      text: 'Source Code'//,
+      //indent: ''
+    }
+  ]
+},
+{
+id: 1,
+image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+user: {
+  name: 'Okinami',
+  handle: 'twitterid',
+  title: 'MasonsGainFormula'
+},
+content: 'Website uses HTML, CSS, Client-Side JavaScript to calculate Mason\'s Gain Formula.',
+links: [
+  {
+    link: 'https://en.wikipedia.org/wiki/Mason%27s_gain_formula#Formula',
+    text: 'Mason\'s Gain Formula Summary'//,
+    //indent: '<br/>'
+  },{
+    link: 'https://lpsa.swarthmore.edu/SFG/',
+    text: 'Production Site'//,
+    //indent: '<br/>'
+  },
+  {
+    link: 'https://vader-coder.github.io/MasonsGainFormula/',
+    text: 'Development Site'//,
+    //indent: '<br/>'
+  },
+  {
+    link: 'https://github.com/vader-coder/MasonsGainFormula',
+    text: 'Source Code'//,
+    //indent: ''
+  }
+]
+},
+{
+id: 3,
+image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  user: {
+    name: 'Philly Bail Statistics',
+    handle: 'twitterid',
+    title: 'Magahi'
+  },
+  content: 'Rudimentary rule-based translation system between [Magahi](https://en.wikipedia.org/wiki/Magahi_language) and English. Built on the apertium pipeline.',
+  links: [{
+    link: 'https://wikis.swarthmore.edu/ling073/Magahi/',
+    text: 'Wiki Page'//,
+    //indent: '<br/>'
+  },
+  {
+    link: 'https://github.com/apertium/apertium-mag',
+    text: 'Source Code'//,
+    //indent: ''
+  }
+
+  {
+  id: 3,
+  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  user: {
+    name: 'Philly Bail Statistics',
+    handle: 'twitterid',
+    title: 'Lead Developer'
+  },
+  content: 'Created an online overview of Philadelphia bail statistics during the one-day hackathon Owlhacks with R Shiny.',
+  links: [{
+      link: 'https://phillybail.shinyapps.io/philly-bail/',
+      text: 'Production Site'//,
+      //indent: '<br/>'
+    },
+    {
+      link: 'https://github.com/dzhen19/philly-bail',
+      text: 'Source Code'//,
+      //indent: ''
+    }
+]
+}
+
+]
+/* https://vuejs.org/v2/guide/list.html */
